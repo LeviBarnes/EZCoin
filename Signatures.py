2fac11325913c079fa5296963b2311b7143cb6bd
LeviBarnes <levi.barnes@gmail.com>
2018-01-12 10:24:47 -0800

    Verifying signature

--- Signatures.py
+++ Signatures.py
@@ -4,6 +4,7 @@ from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from datetime import date
+from cryptography import exceptions as crexceptions
 
 def generate_keys():
     """
@@ -16,8 +17,7 @@ def generate_keys():
         key_size=2048)
 
     # get public key in OpenSSH format
-    public_key = key.public_key().public_bytes(serialization.Encoding.OpenSSH, \
-        serialization.PublicFormat.OpenSSH)
+    public_key = key.public_key();
 
     return key, public_key
 
@@ -37,6 +37,19 @@ def sign(message, private_key):
                                  hashes.SHA256()                                 )
     return signature
 
+def verify(message, signature, public_key):
+    if type(message) == type(b"Hello"):
+        message = message.decode() 
+    try:
+        public_key.verify(signature, bytes(message, 'utf-8'), 
+                          padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                                      salt_length=padding.PSS.MAX_LENGTH),
+                          hashes.SHA256()                                 ) 
+        return True
+    except crexceptions.InvalidSignature:
+        return False
+    
+
 
 #This section will only run if Signatures is invoked directly from the
 #command line. i.e.
@@ -53,7 +66,11 @@ if __name__ == "__main__":
         encryption_algorithm=serialization.NoEncryption())
 
     private_key_str = pem.decode('utf-8')
-    public_key_str = public_key.decode('utf-8')
+    #TODO push this serialization back into generate_keys()
+    #Need to figure a way to deserialize to get a viable _RSAPUblicKey type 
+    #from the public_bytes
+    public_key_str = public_key.public_bytes(serialization.Encoding.OpenSSH, 
+        serialization.PublicFormat.OpenSSH).decode('utf-8')
 
     print('Private key = ') 
     print(private_key_str)
@@ -67,10 +84,22 @@ if __name__ == "__main__":
     print (message)
 
     signature = sign(message, private_key)
-    signature = sign(b"This message is already bytes", private_key)
 
     print()
     print('signature = ')
     print (signature)
+
+    print()
+    if verify(message, signature, public_key):
+        print("String message verified")
+    else:
+        print("Verification of string message FAILED")
+
+    signature = sign(b"This message is already bytes", private_key)
+    if (verify(b"This message is already bytes", signature, public_key)):
+        print("Bytes message verified")
+    else:
+        print("Verification of bytes message FAILED")
+
 
     
