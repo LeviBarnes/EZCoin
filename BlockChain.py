bc2885d9d7361b9b027d3e31aa3e094266f421d4
LeviBarnes <levi.barnes@gmail.com>
2018-01-15 11:15:02 -0800

    Basic blockchain with SHA-256 hashing

---
+++ BlockChain.py
@@ -0,0 +1,26 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.backends import default_backend
+
+class CBlock:
+   data = None
+   previousHash = None
+   def computeHash(self):
+      digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+      #TODO bytes only works for strings?
+      thedata = self.data
+      if type(thedata) == type("Hello"):
+         thedata = bytes(thedata, 'utf-8') 
+      digest.update(thedata)
+      return digest.finalize()
+
+   def __init__(self, _data, previousBlock):
+      self.data = _data
+      if previousBlock != None:
+         previousHash = previousBlock.computeHash()
+
+
+if __name__ == "__main__":
+   root = CBlock("This is the root block", None)
+   b1 = CBlock("This is the second block", root) 
+   b2 = CBlock(b"This block has data in bytes", b1) 
+   b3 = CBlock(445, b2) 
