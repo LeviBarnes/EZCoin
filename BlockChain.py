8cd6faab3b85aecb579d973dc90a7065a3862a63
LeviBarnes <levi.barnes@gmail.com>
2018-01-17 13:44:24 -0800

    Loading and saving blockchains.
    python3 StoreBlockChain.py fails from
    time to time. Run it 4 times to check

--- BlockChain.py
+++ BlockChain.py
@@ -1,9 +1,11 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
+import pickle
 
 class CBlock:
    data = None
    previousHash = None
+   previousBlock = None
    def computeHash(self):
       digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
       #TODO bytes only works for strings?
@@ -16,7 +18,9 @@ class CBlock:
    def __init__(self, _data, previousBlock):
       self.data = _data
       if previousBlock != None:
-         previousHash = previousBlock.computeHash()
+         self.previousHash = previousBlock.computeHash()
+      self.previousBlock = previousBlock
+ 
 
 
 if __name__ == "__main__":
@@ -24,3 +28,8 @@ if __name__ == "__main__":
    b1 = CBlock("This is the second block", root) 
    b2 = CBlock(b"This block has data in bytes", b1) 
    b3 = CBlock(445, b2) 
+
+   myfile = open("blockchain.dat", "wb")
+   pickle.dump(b3, myfile)
+
+   
