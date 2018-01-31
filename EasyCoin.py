c1e704b8f913b4edc64ad2e07f0ed1af925ca9eb
LeviBarnes <levi.barnes@gmail.com>
2018-01-30 21:58:57 -0800

    Moving socket stuff into a module

--- EasyCoin.py
+++ EasyCoin.py
@@ -4,65 +4,20 @@ import sys
 import pickle
 from BlockChain import TxBlock
 from Transactions import Tx
+from EZCoinSocket import EasyCoinManifest, openServerSocket, recvNewBlock
 
-class EasyCoinManifest:
-    def __init__(self, obj):
-        self.kind = type(obj)
-        self.sz = len(pickle.dumps(obj))
-    
 lastBlock = None
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print('starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
-
 recd = 0
-# Listen for incoming connections
-sock.listen(1)
-while True:
-    # Wait for a connection
-    print('waiting for a connection')
-    connection, client_address = sock.accept()
-    all_data = b""
-    try:
-        print('connection from', client_address)
+sSocket = openServerSocket()
 
-        manifest_d = connection.recv(100)
-        manifest = pickle.loads(manifest_d)
-        expected = manifest.sz
-        print ("Expecting a " +str(manifest.kind) + " of size " + str(expected))
+#Recv 3 blocks
+for iter in range(3):
+    newBlock = recvNewBlock(sSocket)
+    newBlock.previousBlock = lastBlock
+    if lastBlock != None:
+        newBlock.previousHash = newBlock.computeHash()
+    lastBlock = newBlock
 
-        connection.send(b"ACK_MFST")
-        data_in = 0
-        # Receive the data in small chunks and retransmit it
-        while data_in < expected:
-            data = connection.recv(16)
-            #print('received {!r}'.format(data))
-            if data:
-                #print('sending data back to the client')
-                #connection.sendall(data)
-                pass
-            else:
-                #print('no data from', client_address)
-                break
-            all_data = all_data + data
-            data_in = data_in + len(data)
-    finally:
-        print ("received " + str(data_in) + " characters. Acknowledging...")
-        connection.send(b"ACK_MMSG")
-        print("Unpickling block ")
-        newBlock = pickle.loads(all_data)
-        newBlock.previousBlock = lastBlock
-        if lastBlock != None:
-            newBlock.previousHash = newBlock.computeHash()
-        lastBlock = newBlock
-        recd = recd + 1
-        if recd > 2:
-            break
 if lastBlock.isvalid():
     print ("Top block is valid.")
 else:
@@ -79,4 +34,3 @@ else:
     print ("ERROR! Root block is invalid")
 # Clean up the connection
         
-connection.close()
